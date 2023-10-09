f84c69f38f5881c61dfdcfc7d9cdcfa4ae017601
Miriam-coded <miriamwmukuhi@gmail.com>
2023-10-09 14:01:23 -0700

    Object-like Macro

---
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -0,0 +1,8 @@
+#include "main.h"
+
+int main(void)
+{
+	int my_array[SIZE];
+
+	return (0);
+}
---
+++ 0x0D-preprocessor/main.h
@@ -0,0 +1,8 @@
+#ifndef HEADER_H
+#define HEADER_H
+
+#define SIZE 1024
+
+int _putchar(char c);
+
+#endif
