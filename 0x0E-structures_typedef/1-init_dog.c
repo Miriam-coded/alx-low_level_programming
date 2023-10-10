e20065951796101afc91f97925314547b4707eef
Miriam-coded <miriamwmukuhi@gmail.com>
2023-10-10 11:07:14 -0700

    A dog is the only thing on earth that loves you more than you love yourself

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,5 +1,4 @@
 #include <stdlib.h>
-#include <stdio.h>
 #include "dog.h"
 
 /**
@@ -20,19 +19,4 @@ void init_dog(struct dog *d, char *name, float age, char *owner)
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
-}
-
-/**
- * main - starts here
- *
- * Return: Always 0
- */
-
-int main(void)
-{
-	struct dog my_dog;
-
-	init_dog(&my_dog, "Poppy", 3.5, "Bob");
-	printf("My name is %s, and I'm %f.1f yrs!\n", my_dog.name, my_dog.age);
-	return (0);
 }
