ea50c231af7ee20b4e0865764c13c3493a3482a3
Miriam-coded <miriamwmukuhi@gmail.com>
2024-02-24 08:38:58 -0800

    Neither irony nor sarcasm is argument

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,73 +1,28 @@
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
 
 /**
- * _atoi - converts a string to an int
- * @s: string to be converted
- *
- * Return: the int converted from the string, or 0 on error
- */
-
-int _atoi(char *s)
-{
-	int n = 0;
-	int sign = 1;
-	int i = 0;
-
-	if (s[0] == '-')
-	{
-		sign = -1;
-		i++;
-	}
-
-	while (s[i] != '\0')
-	{
-		if (s[i] >= '0' && s[i] <= '9')
-		{
-			n = n * 10 + (s[i] - '0');
-		}
-		else
-		{
-			return (0);
-		}
-		i++;
-	}
-
-	return (n * sign);
-}
-
-/**
- * main - prints num1 * num2 results
+ * main - multiplies 2 numbers
  * @argc: number of arguments
- * @argv: array or aguments
- *
- * Return: 0 on success, 1 on error
+ * @argv: array of arguments
+ * 
+ * Return: multiple of the numbers if argc == 3
+ * else returns 1 and print Error
  */
 
 int main(int argc, char *argv[])
 {
-	int num1, num2, ans;
+	int mul = 0;
 
 	if (argc != 3)
 	{
 		printf("Error\n");
-
 		return (1);
 	}
-
-	num1 = _atoi(argv[1]);
-	num2 = _atoi(argv[2]);
-
-	if (num1 == 0 || num2 == 0)
+	else
 	{
-		printf("Error\n");
-
-		return (1);
+		mul = atoi(argv[1]) * atoi(argv[2]);
+		printf("%d\n", mul);
+		return (0);
 	}
-
-	ans = num1 * num2;
-
-	printf("%d\n", ans);
-
-	return (0);
 }
